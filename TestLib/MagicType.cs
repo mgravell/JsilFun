42e4167abd1f5501e9ec732f184391bb9b4ea5cc
Marc Gravell <marc.gravell@gmail.com>
2017-05-26 20:40:16 +0100

    so so confused

---
+++ TestLib/MagicType.cs
@@ -0,0 +1,14 @@
+﻿namespace TestLib
+{
+    public class MagicType
+    {
+        public int Multiply(int x, int y)
+        { // test instance method
+            return x * y;
+        }
+        public static int Add(int x, int y)
+        {// test static int method
+            return x + y;
+        }
+    }
+}
